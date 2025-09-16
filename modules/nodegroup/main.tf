d689481032c9ff17df148c7c8390052caf211940
shivgoyal1999 <shivgoyal1299@gmail.com>
2025-09-16 14:03:06 +0530

    Update main.tf

--- modules/nodegroup/main.tf
+++ modules/nodegroup/main.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_node_group" "this" {
+resource "aws_eks_node_group" "demo" {
   cluster_name    = var.cluster_name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.worker_nodes.arn
