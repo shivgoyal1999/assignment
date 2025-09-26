797fa9824589bd3d452752711e48288181ce7405
shivgoyal1999 <shivgoyal1299@gmail.com>
2025-09-25 21:09:29 -0700

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,6 +22,78 @@ resource "aws_subnet" "private" {
 resource "aws_internet_gateway" "demo" {
   vpc_id = aws_vpc.demo.id
 }
+# ----------------------
+# Internet Gateway
+# ----------------------
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+# ----------------------
+# NAT Gateway (one per AZ for HA, here example with 1)
+# ----------------------
+resource "aws_eip" "nat" {
+  vpc = true
+  tags = {
+    Name = "${var.name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = element(var.public_subnets, 0) # put NAT in first public subnet
+  tags = {
+    Name = "${var.name}-nat-gw"
+  }
+  depends_on = [aws_internet_gateway.this]
+}
+
+# ----------------------
+# Public Route Table
+# ----------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+# Associate each public subnet
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(var.public_subnets, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.name}-private-rt"
+  }
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = element(var.private_subnets, count.index)
+  route_table_id = aws_route_table.private.id
+}
 
 output "vpc_id" {
   value = aws_vpc.demo.id
